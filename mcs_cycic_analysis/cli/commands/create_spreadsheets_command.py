422934d5153bfc4b9a8221e715028fab0dd534c7
Minor Gordon <gordom6@rpi.edu>
2020-12-04 14:39:40 -0500

    output more CSV files

--- mcs_cycic_analysis/cli/commands/create_spreadsheets_command.py
+++ mcs_cycic_analysis/cli/commands/create_spreadsheets_command.py
@@ -4,6 +4,7 @@ from typing import Tuple, Dict, Optional
 
 from mcs_cycic_analysis.cli.commands._command import _Command
 from mcs_cycic_analysis.models.cycic3_label import Cycic3Label
+from mcs_cycic_analysis.models.cycic3_part import Cycic3Part
 from mcs_cycic_analysis.models.cycic3_question import Cycic3Question
 from mcs_cycic_analysis.models.cycic3_sample_dataset import Cycic3SampleDataset
 from mcs_cycic_analysis.paths import DATA_DIR_PATH
@@ -12,9 +13,21 @@ from mcs_cycic_analysis.paths import DATA_DIR_PATH
 class CreateSpreadsheetsCommand(_Command):
     def __call__(self):
         dataset = Cycic3SampleDataset.load()
-        self.__create_entangled_question_pair_csv_file(dataset=dataset)
+        self.__create_all_questions_csv_file(dataset=dataset)
+        self.__create_entangled_question_pairs_csv_file(dataset=dataset)
+        self.__create_part_csv_file(part=dataset.part_a)
+        self.__create_part_csv_file(part=dataset.part_b)
 
-    def __create_entangled_question_pair_csv_file(self, dataset: Cycic3SampleDataset):
+    def __create_all_questions_csv_file(self, dataset: Cycic3SampleDataset):
+        self.__write_csv_file(
+            file_stem="all_questions",
+            row_dicts=tuple(
+                list(self.__get_part_question_columns_dicts(dataset.part_a))
+                + list(self.__get_part_question_columns_dicts(dataset.part_b))
+            ),
+        )
+
+    def __create_entangled_question_pairs_csv_file(self, dataset: Cycic3SampleDataset):
         row_dicts = []
         for entangled_question_pair in dataset.entangled_question_pairs:
             row_dict = {}
@@ -36,6 +49,26 @@ class CreateSpreadsheetsCommand(_Command):
         self.__write_csv_file(
             file_stem="entangled_question_pairs", row_dicts=tuple(row_dicts)
         )
+
+    def __create_part_csv_file(self, *, part: Cycic3Part):
+        self.__write_csv_file(
+            file_stem=f"part_{part.letter}_questions",
+            row_dicts=self.__get_part_question_columns_dicts(part),
+        )
+
+    def __get_part_question_columns_dicts(
+        self, part: Cycic3Part
+    ) -> Tuple[Dict[str, str], ...]:
+        row_dicts = []
+        for question in part.questions:
+            row_dict = {"part": part.letter}
+            row_dict.update(
+                self.__get_question_columns_dict(
+                    question=question, label=part.labels_by_run_id[question.run_id]
+                )
+            )
+            row_dicts.append(row_dict)
+        return tuple(row_dicts)
 
     def __get_question_columns_dict(
         self,
